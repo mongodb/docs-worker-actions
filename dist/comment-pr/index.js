8cf4b8fbd7abf22c17af1c5f71ae7bc49e39cbb8
branberry <brandon.ly@mongodb.com>
2023-11-21 13:13:49 -0600

    [DOP-4003]: Update bundle

--- dist/comment-pr/index.js
+++ dist/comment-pr/index.js
@@ -9636,21 +9636,44 @@ function wrappy (fn, cb) {
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.run = void 0;
 const fs_1 = __importDefault(__nccwpck_require__(7147));
-const core_1 = __importDefault(__nccwpck_require__(2186));
-const github_1 = __nccwpck_require__(5438);
+const core = __importStar(__nccwpck_require__(2186));
+const github = __importStar(__nccwpck_require__(5438));
 // https://docs.github.com/en/actions/security-guides/automatic-token-authentication#about-the-github_token-secret
 async function run() {
-    const prNumber = github_1.context.payload.pull_request?.number;
-    core_1.default.info(`PR number, I think? ${prNumber}`);
+    const prNumber = github.context.payload.pull_request?.number;
+    core.info(`PR number, I think? ${prNumber}`);
     const githubToken = process.env.GITHUB_SECRET;
     if (!githubToken) {
-        core_1.default.error('ERROR! GITHUB_SECRET is not set as an environment variable.');
+        core.error('ERROR! GITHUB_SECRET is not set as an environment variable.');
         return;
     }
     return;
