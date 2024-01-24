baecf2e6fc6d3b470cfcdc5e5833771548afce5b
Matt Meigs <matt.meigs@mongodb.com>
2024-01-24 10:07:51 -0500

    add logs

--- src/build-artifact/run.ts
+++ src/build-artifact/run.ts
@@ -1,29 +1,19 @@
 import fs from 'fs';
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+// import * as core from '@actions/core';
+// import * as github from '@actions/github';
 import path from 'path';
 import { Readable } from 'stream';
 import { finished } from 'stream/promises';
 import readline from 'readline';
-// const readline = require('readline');
 
 
 export async function run(): Promise<void> {
-  // const githubToken = process.env.GITHUB_TOKEN;
-
-  // if (!githubToken) {
-  //   core.error('ERROR! GITHUB_TOKEN is not set as an environment variable.');
-
-  //   throw new Error('Failed. GITHUB_TOKEN is not set.');
-  // }
-
-  // const octokit = github.getOctokit(githubToken);
-
-
-
   try {
+    console.log('in action!!')
     const file = 'output.txt'
     await downloadFile("https://snooty-data-api.mongodb.com/projects/cloud-docs/master/documents", file);
+
+    console.log('downloaded file, I guess')
 
 
     const documents: any[] = [];
@@ -35,9 +25,9 @@ export async function run(): Promise<void> {
     }).on('line', function(line: string) {
       const parsedLine = JSON.parse(line)
       if (parsedLine.type === 'page') {
-        documents.push(parsedLine)
+        documents.push(parsedLine.data)
       } else if (parsedLine.type === 'metadata') {
-        metadata = parsedLine;
+        metadata = parsedLine.data;
       }
     }).on('close', function(){
       const writable = fs.createWriteStream('snooty-documents.js', {flags:'w'});
@@ -46,23 +36,10 @@ export async function run(): Promise<void> {
       metadataWriter.write(JSON.stringify(metadata));
     });
 
-    
+    console.log('now should git clone and run snooty...')
 
-    // const outputsFile = fs.readFileSync('cdk-infra/outputs.json').toString();
-    // const outputs = JSON.parse(outputsFile);
 
-    // const webhook = Object.values(
-    //   outputs[
-    //     `auto-builder-stack-enhancedApp-stg-${process.env.GITHUB_HEAD_REF}-webhooks`
-    //   ]
-    // )[0];
 
-    // await octokit.rest.issues.createComment({
-    //   issue_number: prNumber,
-    //   body: `Your feature branch infrastructure has been deployed! \n\n Your webhook URL is: ${webhook}webhook/githubEndpoint/trigger/build\n\n For more information on how to use this endpoint, follow these [instructions](https://wiki.corp.mongodb.com/x/7FzoDg).`,
-    //   owner: github.context.repo.owner,
-    //   repo: github.context.repo.repo
-    // });
   } catch (error) {
     console.log('Error occurred when retrieving Webhook URL', error);
     throw error;
