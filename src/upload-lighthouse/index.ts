895cd3965da50d73a9ea47f4ed549026bf1015d1
Matt Meigs <matt.meigs@mongodb.com>
2024-05-20 14:54:00 -0400

    typing

--- src/upload-lighthouse/index.ts
+++ src/upload-lighthouse/index.ts
@@ -1,32 +1,9 @@
 import fs from 'fs';
-import * as core from '@actions/core';
 import * as github from '@actions/github';
 import { promisify } from 'util';
 import { MongoClient } from 'mongodb';
 
 const readFileAsync = promisify(fs.readFile);
-
-// import * as chromeLauncher from 'chrome-launcher';
-// import { MongoClient } from "mongodb";
-// import lighthouse from 'lighthouse/core/index.cjs';
-// import { computeMedianRun } from 'lighthouse/core/lib/median-run';
-// import type { Flags } from 'lighthouse';
-// import {
-// getCurrentHash,
-// getCommitTime,
-// getCurrentBranch,
-// getExternalBuildUrl,
-// getCommitMessage,
-// getAuthor,
-// getAvatarUrl,
-// getAncestorHashForBase,
-// getAncestorHashForBranch,
-// } from '@lhci/utils/src/build-context';
-// import { LHServer } from './lh-server';
-// import { LHBuild } from './types/types';
-
-// const DB_NAME = `lighthouse`;
-// const REPOS_COLL_NAME = `reports`;
 
 interface Summary {
   seo: number;
@@ -35,9 +12,30 @@ interface Summary {
   pwa: number;
   accessibility: number;
 }
+const summaryProperties: (keyof Summary)[] = [
+  'seo',
+  'performance',
+  'best-practices',
+  'pwa',
+  'accessibility',
+];
+
+interface Manifest {
+  url: string;
+  isRepresentativeRun: boolean;
+  htmlPath: string;
+  jsonPath: string;
+  summary: Summary;
+}
+
+interface JsonRun {
+  /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
+  [k: string]: any;
+}
 
 const DB_NAME = `lighthouse`;
-const REPOS_COLL_NAME = `reports`;
+const PR_COLL_NAME = `pr_reports`;
+const MAIN_COLL_NAME = `main_reports`;
 
 const getEmptySummary = (): Summary => ({
   seo: 0,
@@ -47,71 +45,72 @@ const getEmptySummary = (): Summary => ({
   accessibility: 0,
 });
 
-const getAverageSummary = (manifests: any[]): Summary => {
+const getAverageSummary = (manifests: Manifest[]): Summary => {
   const summary = getEmptySummary();
-  for (const property of Object.keys(summary)) {
-    console.log('summary prop ', property);
-    console.log('value in reduce ', manifests.reduce((acc, cur) => acc + cur.summary[property], 0))
-    // @ts-ignore
-    summary[property] = (manifests.reduce((acc, cur) => acc + cur.summary[property], 0) / manifests.length)
+  for (const property of summaryProperties) {
+    summary[property] =
+      manifests.reduce((acc, cur) => acc + cur.summary[property], 0) /
+      manifests.length;
   }
   return summary;
 };
 
-const getRuns = async (manifests: any[]): Promise<{ jsonRuns: any[], htmlRuns: string[] }> => {
+const getRuns = async (
+  manifests: Manifest[],
+): Promise<{ jsonRuns: JsonRun[]; htmlRuns: string[] }> => {
   const jsonRuns = [];
   const htmlRuns = [];
 
   for (const manifest of manifests) {
-    jsonRuns.push(JSON.parse((
-      await readFileAsync(manifest.jsonPath)
-    ).toString()));
+    jsonRuns.push(
+      JSON.parse((await readFileAsync(manifest.jsonPath)).toString()),
+    );
 
-    htmlRuns.push((
-      await readFileAsync(manifest.htmlPath)
-    ).toString());
+    htmlRuns.push((await readFileAsync(manifest.htmlPath)).toString());
   }
 
   await Promise.all(jsonRuns);
   await Promise.all(htmlRuns);
   return { jsonRuns, htmlRuns };
-}
+};
 
 async function main(): Promise<void> {
   const commitHash = github.context.sha;
   const author = github.context.actor;
-  console.log('Commit Message from context... ', process.env.COMMIT_MESSAGE);
-  console.log('timestamp from context... ', process.env.COMMIT_TIMESTAMP);
-  // TODO: Need Commit Message...
-  // Keep trying ${{ github.event.head_commit.message }}
-  // TODO: Branch name
-  // TODO: If merge, set that as a property - OR put in a different database?
+  const commitMessage = process.env.COMMIT_MESSAGE || '';
+  const commitTimestamp = process.env.COMMIT_TIMESTAMP || '';
+  const project = process.env.PROJECT_TO_BUILD || '';
+  const branch = process.env.BRANCH_NAME || '';
 
   try {
     const outputsFile = (
       await readFileAsync('./lhci/manifest.json')
     ).toString();
-    // @ts-ignore
-    const manifestsOfLighthouseRuns: any[] = JSON.parse(outputsFile);
 
-    console.log('OUTPUT of manifest json ', manifestsOfLighthouseRuns);
-    
-    // @ts-ignore
-    const [desktopRunManifests, mobileRunManifests] = manifestsOfLighthouseRuns.reduce((acc, cur) => {
-      if (cur.url.includes('?desktop')) acc[0].push(cur);
-      else acc[1].push(cur);
-      return acc;
-    }, [[], []] as any[][]);
-    
+    const manifestsOfLighthouseRuns: Manifest[] = JSON.parse(outputsFile);
+
+    const [desktopRunManifests, mobileRunManifests] =
+      manifestsOfLighthouseRuns.reduce(
+        (acc, cur) => {
+          if (cur.url.includes('?desktop')) acc[0].push(cur);
+          else acc[1].push(cur);
+          return acc;
+        },
+        [[], []] as Manifest[][],
+      );
+
     const urlTested = mobileRunManifests[0].url;
     const desktopSummary = getAverageSummary(desktopRunManifests);
-    const { htmlRuns: desktopHtmlRuns, jsonRuns: desktopJsonRuns } = await getRuns(desktopRunManifests);
+    const { htmlRuns: desktopHtmlRuns, jsonRuns: desktopJsonRuns } =
+      await getRuns(desktopRunManifests);
 
     const desktopRunDocument = {
       commitHash,
+      commitMessage,
+      commitTimestamp,
       author,
-      project: process.env.PROJECT_TO_BUILD,
-      branch: process.env.BRANCH_NAME,
+      project,
+      branch,
       url: urlTested,
       summary: desktopSummary,
       htmlRuns: desktopHtmlRuns,
@@ -120,13 +119,16 @@ async function main(): Promise<void> {
     };
 
     const mobileSummary = getAverageSummary(mobileRunManifests);
-    const { htmlRuns: mobileHtmlRuns, jsonRuns: mobileJsonRuns } = await getRuns(mobileRunManifests);
+    const { htmlRuns: mobileHtmlRuns, jsonRuns: mobileJsonRuns } =
+      await getRuns(mobileRunManifests);
 
     const mobileRunDocument = {
       commitHash,
+      commitMessage,
+      commitTimestamp,
       author,
-      project: process.env.PROJECT_TO_BUILD,
-      branch: process.env.BRANCH_NAME,
+      project,
+      branch,
       url: urlTested,
       summary: mobileSummary,
       htmlRuns: mobileHtmlRuns,
@@ -134,15 +136,15 @@ async function main(): Promise<void> {
       type: 'mobile',
     };
 
-
+    console.log('Uploading to MongoDB...');
+    const collectionName = branch === 'main' ? MAIN_COLL_NAME : PR_COLL_NAME;
     const client = new MongoClient(process.env.ATLAS_URI || '');
     const db = client.db(DB_NAME);
 
-    const collection = db.collection(REPOS_COLL_NAME);
-    const insertionMessage = await collection.insertMany([desktopRunDocument, mobileRunDocument]);
-    console.log('insertion ', insertionMessage);
+    const collection = db.collection(collectionName);
+    await collection.insertMany([desktopRunDocument, mobileRunDocument]);
 
-    console.log('Closing');
+    console.log('Closing database connection');
     await client.close();
     return;
   } catch (error) {
