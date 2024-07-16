dd0d6c057411995c30f75526c0f2c91140cac315
mmeigs <matt.meigs@mongodb.com>
2024-07-16 16:02:50 -0400

    package

--- dist/upload-lighthouse/index.js
+++ dist/upload-lighthouse/index.js
@@ -72222,7 +72222,6 @@ var exports = __webpack_exports__;
 // import { MongoClient } from 'mongodb';
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const client_s3_1 = __nccwpck_require__(9250);
-const fs_1 = __nccwpck_require__(7147);
 const mongodb_1 = __nccwpck_require__(8821);
 const DB_NAME = `lighthouse`;
 const COLL_NAME = `main_reports`;
@@ -72233,10 +72232,12 @@ async function upload({ htmlRuns, branch, url, type, commitHash, }) {
     const urlWithoutType = url.endsWith('?desktop') ? url.slice(0, -8) : url;
     const urlWithDashes = urlWithoutType.split(/\/\?|\//).join('-');
     const destinationDir = `/${reportType}/${commitHash}/${urlWithDashes}/${type}`;
+    console.log('destinationDir', destinationDir);
     const uploads = htmlRuns.map(async (htmlReport, i) => {
         const key = `${destinationDir}/${i + 1}.html`;
         const input = {
-            Body: (0, fs_1.createReadStream)(htmlReport),
+            // Body: createReadStream(htmlReport),
+            Body: 'Body!!! ',
             Key: key,
             Bucket: awsBucket,
             ContentType: 'text/html',
